c1f0ea0d5bd18b918169970fcb11b10119c1c0ea
andre <andrelaprov@gmail.com>
2020-06-12 23:01:48 -0400

    sent

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  let num1 = +req.body.num1;
+  let num2 = +req.body.num2;
+
+  let result = num1 + num2;
+
+  res.send(`The result is ${result}`);
+});
+
+let port = 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port} !!!`));
